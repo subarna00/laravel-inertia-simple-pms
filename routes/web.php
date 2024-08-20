c03fa04179eed62caafe26cb7c569822cf96e417
pro <pro@pros-MacBook-Pro.local>
2024-08-20 08:09:23 +0545

    inerttia and breeze setup

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::redirect('/', '/dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
+
+    Route::resource('project', ProjectController::class);
+    Route::resource('task', ProjectController::class);
+    Route::resource('user', ProjectController::class);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
